752a45faa4de210b0b37b1c75c16bb90f00f8fb0
Andréa <andreamaragao@gmail.com>
2023-03-13 22:37:01 -0300

    Adicionar arquivos de projeto.

---
+++ Entidade/Fruta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto04.Entidade
+{
+    public class Fruta
+    {
+
+        public string Nome { get; set; }
+
+        public string Cor { get; set;}
+
+        public int Tamanho { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Projeto04.Entidade;
+using System;
+
+namespace Projeto04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruta fruta1 = new Fruta();
+            fruta1.Nome = "morango";
+            fruta1.Cor = "vermelho";
+            fruta1.Tamanho = 10;
+
+            Fruta fruta2;
+            fruta2 = new Fruta();
+            fruta2.Nome = "cupuacu";
+            fruta2.Cor = "amarelo";
+            fruta2.Tamanho = 30;
+
+            Fruta fruta3;
+            fruta3 = new Fruta();
+            fruta3.Nome = "manga";
+            fruta3.Cor = "amarela";
+            fruta3.Tamanho = 25;
+
+            Fruta fruta4= new Fruta();
+            fruta4.Nome = "kiwi";
+            fruta4.Cor = "verde";
+            fruta4.Tamanho = 7;
+
+
+
+
+        }
+    }
+}
